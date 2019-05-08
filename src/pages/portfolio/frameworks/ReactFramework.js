fe802e0beda95899a831a94a67f262f976321cd3
angel <angelsol299@hotmail.com>
2019-05-08 23:40:58 +0200

    adding portfolio

---
+++ src/pages/portfolio/frameworks/ReactFramework.js
@@ -0,0 +1,70 @@
+import React from "react";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCol
+} from "mdbreact";
+
+const ReactFremework = () => {
+  return (
+    <>
+      <MDBCol sm="4">
+        <MDBCard style={{ width: "22rem" }}>
+          <MDBCardImage
+            className="img-fluid"
+            src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg"
+            waves
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              Some quick example text to build on the card title and make up the
+              bulk of the card&apos;s content.
+            </MDBCardText>
+            <MDBBtn href="#">MDBBtn</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol sm="4">
+        <MDBCard style={{ width: "22rem" }}>
+          <MDBCardImage
+            className="img-fluid"
+            src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg"
+            waves
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              Some quick example text to build on the card title and make up the
+              bulk of the card&apos;s content.
+            </MDBCardText>
+            <MDBBtn href="#">MDBBtn</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+      <MDBCol sm="4">
+        <MDBCard style={{ width: "22rem" }}>
+          <MDBCardImage
+            className="img-fluid"
+            src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg"
+            waves
+          />
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText>
+              Some quick example text to build on the card title and make up the
+              bulk of the card&apos;s content.
+            </MDBCardText>
+            <MDBBtn href="#">MDBBtn</MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+    </>
+  );
+};
+
+export default ReactFremework;
